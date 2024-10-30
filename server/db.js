d0aba87cc85f24878bb1ddbbfe956d7e29fff2b5
frhandev <frhandev@gmail.com>
2024-10-30 08:37:46 -0400

    db.js changes

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:EHOxMdYQiodAa3jv8D8StZDU@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
